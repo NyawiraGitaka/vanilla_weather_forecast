c084ebf634cb8a0c1dcf12da336923bdd338b9b9
NyawiraGitaka <nyawira.gitaka@gmail.com>
2023-12-15 19:32:30 +0000

    Integrating API to display forecast days and weather icons

--- src/app.js
+++ src/app.js
@@ -56,32 +56,40 @@ function getCity(event) {
 
   searchCity(searchInput.value);
 }
-function getForecast(city){
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
   let apiKey = "314o3fa79375c2341f0ct1d3593ab8a2";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-  
 }
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  let forecastHtml = " ";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` <div class="weather-forecast-day">
-          <div class="weather-forecast-date"> ${day} </div>
-          <div class="weather-forecast-icon"> 🌤️</div>
+ let forecastHtml = " ";
+ 
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` <div class="weather-forecast-day">
+          <div class="weather-forecast-date"> ${formatDay(day.time)}</div>
+          <div> <img class="weather-forecast-icon" src="${day.condition.icon_url}">
+          </div>
           <div class="weather-forecast-temps">
             <span class="forecast-weather-max">
-              18°
-            </span>
+              ${Math.round(day.temperature.maximum)}°
+            </span> 
             <span class="forecast-weather-min">
-              12°
+              ${Math.round(day.temperature.minimum)}°
             </span>
           </div>
         </div>`;
+    }
   });
   let forecastElement = document.querySelector("#weather-forecast");
   forecastElement.innerHTML = forecastHtml;
@@ -90,5 +98,3 @@ function displayForecast(response) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", getCity);
 searchCity("Tallinn");
-
-
